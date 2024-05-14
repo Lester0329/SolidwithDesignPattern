1845ea6e60c7c7f1596c44d3a97282313f62b854
Lester0329 <72334101+Lester0329@users.noreply.github.com>
2024-05-14 22:06:31 +0800

    Update ProgramRun.java

--- ProgramRun.java
+++ ProgramRun.java
@@ -1,14 +1,14 @@
 public class ProgramRun {
     public static void main(String[] args) {
 
-        Student ron = new Student("Ron Anthony Sy");
+        Student NEU = new Student("Mark Lester Laurente");
 
         Book book = new Book("Hello World");
         Journal journal = new Journal("The History of Programming Languages");
         Article article = new Article("What is Java Programming Language");
 
-        ron.borrowResource(book);
-        ron.borrowResource(journal);
-        ron.borrowResource(article);
+        NEU.borrowResource(book);
+        NEU.borrowResource(journal);
+        NEU.borrowResource(article);
     }
 }
